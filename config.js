fc91c9aae7ae6fc6c41db9671c73eb3ad6c49e2c
gstutorial <gobind.bngn@gmail.com>
2026-05-30 00:42:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Global Configuration File
 const GS_CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbyGaGUwlVjopz1x199ejZEGBGyP6IWZnsDYKzH9zPEAI-WEHIyjFTiaTDljHgmc8xOZBA/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbwlh_WP_AY6Cucn7xK1w80EE28-iKcS5lO0-CKrpmErylo7z-HW76rlPbnmMXkCGnDdjw/exec'
 };
